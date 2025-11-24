c76f0943503ab986fc9c065173bc88afa2e78d6b
Yves Tumushimire <yvestumushimire@gmail.com>
2025-11-24 19:39:12 +0200

    finish video aspect assignment
    Signed-off-by: Yves Tumushimire <yvestumushimire@gmail.com>

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -39,18 +39,18 @@ export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
   const fileName = `${videoId}.${getFileExtensionFromMediaType(rawVideo.type)}`;
   const filePath = path.join(cfg.assetsRoot, fileName);
   await Bun.write(filePath, rawVideo);
+  const aspectRatio = await getVideoAspectRatio(filePath);
+  const awsfileName = `${aspectRatio}/${fileName}`;
   const videoFile = Bun.file(filePath);
   const videoIdUuid = parse(videoId);
-  const s3file = cfg.s3Client.file(fileName, {
+  const s3file = cfg.s3Client.file(awsfileName, {
     bucket: cfg.s3Bucket,
     // endpoint: `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/`,
   });
   await s3file.write(videoFile, { type: rawVideo.type });
   await videoFile.delete();
-  video.videoURL = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${fileName}`;
-  console.log(
-    `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${fileName}`,
-  );
+  video.videoURL = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${awsfileName}`;
+
   updateVideo(cfg.db, video);
   return respondWithJSON(200, video);
 }
@@ -58,4 +58,47 @@ export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
 function getFileExtensionFromMediaType(mediaType: string): string {
   const parts = mediaType.split("/");
   return parts.length === 2 ? parts[1] : "bin";
+}
+
+async function getVideoAspectRatio(filePath: string) {
+  const process = Bun.spawn(
+    [
+      "ffprobe",
+      "-v",
+      "error",
+      "-select_streams",
+      "v:0",
+      "-show_entries",
+      "stream=width,height",
+      "-of",
+      "json",
+      filePath,
+    ],
+    {
+      stdout: "pipe",
+      stderr: "pipe",
+    },
+  );
+
+  const outputText = await new Response(process.stdout).text();
+  const errorText = await new Response(process.stderr).text();
+
+  const exitCode = await process.exited;
+
+  if (exitCode !== 0) {
+    throw new Error(`ffprobe error: ${errorText}`);
+  }
+
+  const output = JSON.parse(outputText);
+  if (!output.streams || output.streams.length === 0) {
+    throw new Error("No video streams found");
+  }
+
+  const { width, height } = output.streams[0];
+
+  return width === Math.floor(16 * (height / 9))
+    ? "landscape"
+    : height === Math.floor(16 * (width / 9))
+      ? "portrait"
+      : "other";
 }
